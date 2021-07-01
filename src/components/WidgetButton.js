26f4acd0da9c340d096f2285864d1c5e823ffae8
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-07-01 14:05:56 -0500

    Update WidgetButton.js

--- src/components/WidgetButton.js
+++ src/components/WidgetButton.js
@@ -1,23 +1,22 @@
 import React from 'react'
 
-const WidgetButton = ({ myWidgetFunction, style, buttonText }) => (
-  <>
-    <button
-      id='cloudinary_upload_button'
-      style={
-        style || {
-          color: 'white',
-          border: 'none',
-          width: '120px',
-          backgroundColor: 'green',
-          borderRadius: '4px',
-          height: '25px'
-        }
+const WidgetButton = React.forwardRef(({ myWidgetFunction, style, buttonText }, ref) => (
+  <button
+    ref={ref}
+    id='cloudinary_upload_button'
+    style={
+      style || {
+        color: 'white',
+        border: 'none',
+        width: '120px',
+        backgroundColor: 'green',
+        borderRadius: '4px',
+        height: '25px'
       }
-      onClick={() => myWidgetFunction()}
-    >
-      {buttonText || 'Upload files'}
-    </button>
-  </>
-)
+    }
+    onClick={() => myWidgetFunction()}
+  >
+    {buttonText || 'Upload files'}
+  </button>
+))
 export default WidgetButton
